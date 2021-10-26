13a8a122eb3b4e55c07f0a8551e7bb4e966c2036
kemalkagankural <66682123+kemalkagankural@users.noreply.github.com>
2021-10-26 20:47:37 +0300

    Counter App

---
+++ counter/app.js
@@ -0,0 +1,16 @@
+let number = 0
+let numberDOM = document.querySelector("#number")
+let increaseDOM = document.querySelector("#increase")
+let decreaseDOM = document.querySelector("#decrease")
+numberDOM.innerHTML = number
+
+
+increaseDOM.addEventListener("click", clickEvent)
+decreaseDOM.addEventListener("click", clickEvent)
+function clickEvent() {
+  if (this.id == "increase") {
+    numberDOM.innerHTML = number += 1;
+  } else{
+    numberDOM.innerHTML = number -= 1;
+  }
+}
